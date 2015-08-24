338ff55dcd056c8bc8adf23306b27269173b1f04
Dan Kashkovsky <dannie.k@me.com>
2015-08-24 05:51:06 -0700

    Matrix to triangular look, calculate determinant

--- SLAEByGauss/Form1.Designer.cs
+++ SLAEByGauss/Form1.Designer.cs
@@ -28,12 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.a11 = new System.Windows.Forms.TextBox();
+            this.a12 = new System.Windows.Forms.TextBox();
+            this.a13 = new System.Windows.Forms.TextBox();
+            this.a14 = new System.Windows.Forms.TextBox();
+            this.a15 = new System.Windows.Forms.TextBox();
+            this.a25 = new System.Windows.Forms.TextBox();
+            this.a24 = new System.Windows.Forms.TextBox();
+            this.a23 = new System.Windows.Forms.TextBox();
+            this.a22 = new System.Windows.Forms.TextBox();
+            this.a21 = new System.Windows.Forms.TextBox();
+            this.a35 = new System.Windows.Forms.TextBox();
+            this.a34 = new System.Windows.Forms.TextBox();
+            this.a33 = new System.Windows.Forms.TextBox();
+            this.a32 = new System.Windows.Forms.TextBox();
+            this.a31 = new System.Windows.Forms.TextBox();
+            this.a45 = new System.Windows.Forms.TextBox();
+            this.a44 = new System.Windows.Forms.TextBox();
+            this.a43 = new System.Windows.Forms.TextBox();
+            this.a42 = new System.Windows.Forms.TextBox();
+            this.a41 = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.monitor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // a11
+            // 
+            this.a11.Location = new System.Drawing.Point(43, 38);
+            this.a11.Name = "a11";
+            this.a11.Size = new System.Drawing.Size(43, 20);
+            this.a11.TabIndex = 0;
+            this.a11.Text = "-3";
+            // 
+            // a12
+            // 
+            this.a12.Location = new System.Drawing.Point(116, 38);
+            this.a12.Name = "a12";
+            this.a12.Size = new System.Drawing.Size(43, 20);
+            this.a12.TabIndex = 1;
+            this.a12.Text = "4";
+            // 
+            // a13
+            // 
+            this.a13.Location = new System.Drawing.Point(186, 38);
+            this.a13.Name = "a13";
+            this.a13.Size = new System.Drawing.Size(43, 20);
+            this.a13.TabIndex = 2;
+            this.a13.Text = "1";
+            // 
+            // a14
+            // 
+            this.a14.Location = new System.Drawing.Point(257, 38);
+            this.a14.Name = "a14";
+            this.a14.Size = new System.Drawing.Size(43, 20);
+            this.a14.TabIndex = 3;
+            this.a14.Text = "4";
+            // 
+            // a15
+            // 
+            this.a15.Location = new System.Drawing.Point(327, 38);
+            this.a15.Name = "a15";
+            this.a15.Size = new System.Drawing.Size(43, 20);
+            this.a15.TabIndex = 4;
+            this.a15.Text = "-1";
+            // 
+            // a25
+            // 
+            this.a25.Location = new System.Drawing.Point(327, 84);
+            this.a25.Name = "a25";
+            this.a25.Size = new System.Drawing.Size(43, 20);
+            this.a25.TabIndex = 9;
+            this.a25.Text = "-1";
+            // 
+            // a24
+            // 
+            this.a24.Location = new System.Drawing.Point(257, 84);
+            this.a24.Name = "a24";
+            this.a24.Size = new System.Drawing.Size(43, 20);
+            this.a24.TabIndex = 8;
+            this.a24.Text = "2";
+            // 
+            // a23
+            // 
+            this.a23.Location = new System.Drawing.Point(186, 84);
+            this.a23.Name = "a23";
+            this.a23.Size = new System.Drawing.Size(43, 20);
+            this.a23.TabIndex = 7;
+            this.a23.Text = "3";
+            // 
+            // a22
+            // 
+            this.a22.Location = new System.Drawing.Point(116, 84);
+            this.a22.Name = "a22";
+            this.a22.Size = new System.Drawing.Size(43, 20);
+            this.a22.TabIndex = 6;
+            this.a22.Text = "1";
+            // 
+            // a21
+            // 
+            this.a21.Location = new System.Drawing.Point(43, 84);
+            this.a21.Name = "a21";
+            this.a21.Size = new System.Drawing.Size(43, 20);
+            this.a21.TabIndex = 5;
+            this.a21.Text = "0";
+            // 
+            // a35
+            // 
+            this.a35.Location = new System.Drawing.Point(327, 130);
+            this.a35.Name = "a35";
+            this.a35.Size = new System.Drawing.Size(43, 20);
+            this.a35.TabIndex = 14;
+            this.a35.Text = "4";
+            // 
+            // a34
+            // 
+            this.a34.Location = new System.Drawing.Point(257, 130);
+            this.a34.Name = "a34";
+            this.a34.Size = new System.Drawing.Size(43, 20);
+            this.a34.TabIndex = 13;
+            this.a34.Text = "-3";
+            // 
+            // a33
+            // 
+            this.a33.Location = new System.Drawing.Point(186, 130);
+            this.a33.Name = "a33";
+            this.a33.Size = new System.Drawing.Size(43, 20);
+            this.a33.TabIndex = 12;
+            this.a33.Text = "-2";
+            // 
+            // a32
+            // 
+            this.a32.Location = new System.Drawing.Point(116, 130);
+            this.a32.Name = "a32";
+            this.a32.Size = new System.Drawing.Size(43, 20);
+            this.a32.TabIndex = 11;
+            this.a32.Text = "0";
+            // 
+            // a31
+            // 
+            this.a31.Location = new System.Drawing.Point(43, 130);
+            this.a31.Name = "a31";
+            this.a31.Size = new System.Drawing.Size(43, 20);
+            this.a31.TabIndex = 10;
+            this.a31.Text = "4";
+            // 
+            // a45
+            // 
+            this.a45.Location = new System.Drawing.Point(327, 175);
+            this.a45.Name = "a45";
+            this.a45.Size = new System.Drawing.Size(43, 20);
+            this.a45.TabIndex = 19;
+            this.a45.Text = "-2";
+            // 
+            // a44
+            // 
+            this.a44.Location = new System.Drawing.Point(257, 175);
+            this.a44.Name = "a44";
+            this.a44.Size = new System.Drawing.Size(43, 20);
+            this.a44.TabIndex = 18;
+            this.a44.Text = "-5";
+            // 
+            // a43
+            // 
+            this.a43.Location = new System.Drawing.Point(186, 175);
+            this.a43.Name = "a43";
+            this.a43.Size = new System.Drawing.Size(43, 20);
+            this.a43.TabIndex = 17;
+            this.a43.Text = "1";
+            // 
+            // a42
+            // 
+            this.a42.Location = new System.Drawing.Point(116, 175);
+            this.a42.Name = "a42";
+            this.a42.Size = new System.Drawing.Size(43, 20);
+            this.a42.TabIndex = 16;
+            this.a42.Text = "3";
+            // 
+            // a41
+            // 
+            this.a41.Location = new System.Drawing.Point(43, 175);
+            this.a41.Name = "a41";
+            this.a41.Size = new System.Drawing.Size(43, 20);
+            this.a41.TabIndex = 15;
+            this.a41.Text = "1000";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(154, 231);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 20;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // monitor
+            // 
+            this.monitor.Location = new System.Drawing.Point(394, 38);
+            this.monitor.Multiline = true;
+            this.monitor.Name = "monitor";
+            this.monitor.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.monitor.Size = new System.Drawing.Size(346, 328);
+            this.monitor.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 378);
+            this.Controls.Add(this.monitor);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.a45);
+            this.Controls.Add(this.a44);
+            this.Controls.Add(this.a43);
+            this.Controls.Add(this.a42);
+            this.Controls.Add(this.a41);
+            this.Controls.Add(this.a35);
+            this.Controls.Add(this.a34);
+            this.Controls.Add(this.a33);
+            this.Controls.Add(this.a32);
+            this.Controls.Add(this.a31);
+            this.Controls.Add(this.a25);
+            this.Controls.Add(this.a24);
+            this.Controls.Add(this.a23);
+            this.Controls.Add(this.a22);
+            this.Controls.Add(this.a21);
+            this.Controls.Add(this.a15);
+            this.Controls.Add(this.a14);
+            this.Controls.Add(this.a13);
+            this.Controls.Add(this.a12);
+            this.Controls.Add(this.a11);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox a11;
+        private System.Windows.Forms.TextBox a12;
+        private System.Windows.Forms.TextBox a13;
+        private System.Windows.Forms.TextBox a14;
+        private System.Windows.Forms.TextBox a15;
+        private System.Windows.Forms.TextBox a25;
+        private System.Windows.Forms.TextBox a24;
+        private System.Windows.Forms.TextBox a23;
+        private System.Windows.Forms.TextBox a22;
+        private System.Windows.Forms.TextBox a21;
+        private System.Windows.Forms.TextBox a35;
+        private System.Windows.Forms.TextBox a34;
+        private System.Windows.Forms.TextBox a33;
+        private System.Windows.Forms.TextBox a32;
+        private System.Windows.Forms.TextBox a31;
+        private System.Windows.Forms.TextBox a45;
+        private System.Windows.Forms.TextBox a44;
+        private System.Windows.Forms.TextBox a43;
+        private System.Windows.Forms.TextBox a42;
+        private System.Windows.Forms.TextBox a41;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox monitor;
     }
 }
 
--- SLAEByGauss/Form1.cs
+++ SLAEByGauss/Form1.cs
@@ -12,9 +12,46 @@ namespace SLAEByGauss
 {
     public partial class Form1 : Form
     {
+        int[,] matrix;
+        Solver solver;
         public Form1()
         {
             InitializeComponent();
         }
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            matrix = new int[4,5];
+            
+            if (int.TryParse(a11.Text, out matrix[0,0]) &&
+                int.TryParse(a12.Text, out matrix[0,1]) &&
+                int.TryParse(a13.Text, out matrix[0,2]) &&
+                int.TryParse(a14.Text, out matrix[0,3]) &&
+                int.TryParse(a15.Text, out matrix[0,4]) &&
+                int.TryParse(a21.Text, out matrix[1,0]) &&
+                int.TryParse(a22.Text, out matrix[1,1]) &&
+                int.TryParse(a23.Text, out matrix[1,2]) &&
+                int.TryParse(a24.Text, out matrix[1,3]) &&
+                int.TryParse(a25.Text, out matrix[1,4]) &&
+                int.TryParse(a31.Text, out matrix[2,0]) &&
+                int.TryParse(a32.Text, out matrix[2,1]) &&
+                int.TryParse(a33.Text, out matrix[2,2]) &&
+                int.TryParse(a34.Text, out matrix[2,3]) &&
+                int.TryParse(a35.Text, out matrix[2,4]) &&
+                int.TryParse(a41.Text, out matrix[3,0]) &&
+                int.TryParse(a42.Text, out matrix[3,1]) &&
+                int.TryParse(a43.Text, out matrix[3,2]) &&
+                int.TryParse(a44.Text, out matrix[3,3]) &&
+                int.TryParse(a45.Text, out matrix[3,4])
+                )
+            {
+                solver = new Solver(matrix, monitor);
+            }
+            else
+            {
+                monitor.Text = "Wrong input. Integer values expected.";
+            }
+        }
+
+        
     }
 }
